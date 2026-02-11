67e71de73dd9d6c38c62d4ce6f37a95654f5a19f
Daysof1 <dayana_almeciga@soy.sena.edu.co>
2026-02-11 16:41:53 -0500

    Dayana

--- backend/models/Subcategoria.js
+++ backend/models/Subcategoria.js
@@ -1,7 +1,7 @@
 /**
- * MODELO CATEGORIA
- * define la tabla Categoria en la base de daos
- * Almacena las categorias principales de los productos
+ * MODELO SUBCATEGORIA
+ * define la tabla Subcategoria en la base de daos
+ * Almacena las subcategorias de los productos
  */
 
 //Importar DataType de sequelize 
@@ -11,9 +11,9 @@ const {DataTypes } = require('sequelize');
 const { sequelize } = require('../config/database');
 
 /**
- * Definir el modelo de Categoria
+ * Definir el modelo de Subcategoria
  */
-const Categoria = sequelize.define('Categoria', {
+const Subcategoria = sequelize.define('Subcategoria', {
     //Campos de la tablas
     //Id Identificador unico (PRIMARY KEY)
     id: {
@@ -27,26 +27,40 @@ const Categoria = sequelize.define('Categoria', {
         type: DataTypes.STRING(100), 
         allowNull: false,
         unique: {
-            msg: 'Ya existe una categoria con ese nombre'
+            msg: 'Ya existe una subcategoria con ese nombre'
         },
         validate: {
-            notEmpy: {
-                msg: 'El nombre de la categoria no puede estar vacio'
+            notEmpty: {
+                msg: 'El nombre de la subcategoria no puede estar vacio'
             },
             len: {
                 args: [2, 100],
-                msg: 'El nombre de la categoria debe tener netre 2 y 100 caracteres'
+                msg: 'El nombre debe tener netre 2 y 100 caracteres'
             }
         }
     },
 
     /**
-     * Descripcion dee la categoria 
+     * Descripcion dee la subcategoria 
      */
     descripcion: {
         type: DataTypes.TEXT,
         allowNull: true,
     },
+
+    /**
+     * categoriaId - ID de la categoria a la que pertenece (FOREIGN KEY)
+     * Esta es la relacion con la tabla categoria
+     */
+    categoriaId: {
+        type: DataTypes.INTERGER,
+        allowNull: false,
+        references: {
+            model: 'categorias', //nombre de la tabla relacionada
+            key: 'id' //campo de la tabla relacioonada
+        },
+        
+    }
 
     /**
      * activo estado de la categoria 
